a29b61693442369857706e5824759a7094b1f544
Sampurn Rattan <sampurn@192.168.1.9>
2026-01-09 18:30:07 +0530

    Adding model fitting and prediction

--- model.py
+++ model.py
@@ -2,5 +2,13 @@ import pandas as pd
 import sklearn
 
 from sklearn.datasets import load_iris
-
 data = load_iris()
+X = data.features
+y = data.target
+
+from sklearn.linear import LogisticRegression
+
+model = LogisticRegression()
+model.fit(X,y)
+
+print(model.predict(X))
